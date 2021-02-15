b7fc5a6edddf8b277261450f74f1e26f00ce0d95
Carlos A. Chávez Rosario <66329126+cchavezmx@users.noreply.github.com>
2021-02-14 20:38:23 -0600

    cambio en el control de mensajes

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -4,12 +4,12 @@ module.exports = {
     create: async(req, res ) => {
 
         const { message, name, phone, email } = req.body
-        const messageBody = `El cliente: ${name}, requiere más infomarción de la empresa, su número teléfonico es: ${phone}, correo: ${email}, y el mensaje que dejo en la página: ${message}`
+        const messageBody = `El cliente: ${name}, requiere más infomarción de la empresa, su número teléfonico es: ${phone}, y el mensaje que dejo en la página: ${message}`
 
         try {
 
             const twilioMessage = await TwilioService.send(messageBody)
-            console.log(twilioMessage, 'res')
+            
             if(twilioMessage){
                 res.status(200).json(twilioMessage)
             }
