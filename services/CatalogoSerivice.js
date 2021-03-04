1370f41a54991779ad094b80cc8e5f325a5f3b76
Carlos A. Chávez Rosario <66329126+cchavezmx@users.noreply.github.com>
2021-03-04 01:25:37 -0600

    se añadio regex a la busqueda global de todos los productos

--- services/CatalogoSerivice.js
+++ services/CatalogoSerivice.js
@@ -1,22 +1,46 @@
 const { Catalogo, Label, Brand  } = require('../models/')
 
 module.exports =  {
+    
+    findByQueryText: async(text) => {
 
-    findByQueryText: (text) => {
-
-        const queryText = Catalogo.aggregate().match(
-            {
-                '$text': {
-                    '$search': text
-                }
-            }
-        ).sort({
-                "model": { "$meta": "textScore" }
-            }
+        // buscamos primero por text index
+        
+        const p1 = new Promise(( resolve, _ ) => {
+            resolve(
+                Catalogo.aggregate().match(
+                    {
+                        '$text': {
+                            '$search': text
+                        }
+                    }
+                ).sort({
+                        "model": { "$meta": "textScore" }
+                    }
+                    )
             )
-            
-        return queryText
+        })
 
+        // buscamos por regex
+
+        const p2 = new Promise((resolve, _ ) => {
+            resolve(
+                Catalogo.aggregate().match(
+                    {
+                        "title": { "$regex": new RegExp(`${text}`) }
+                    }
+                )
+            )
+        }) 
+
+        // unimos las dos busquedas en un solo object
+
+        return Promise.all([ p1, p2 ]).then( res => {
+
+            let resultArra = Object.assign(res[0], res[1])
+            return resultArra
+        })
+            
     },
 
     create: (props) => new Catalogo(props).save(),
@@ -30,9 +54,9 @@ module.exports =  {
         return response
     },
     getAllProducts: () => Catalogo.find({ isActive: true }),
-    sample: () => {
+    sample: async() => {
 
-        const p1 = new Promise((resolve, reject) => {
+        const p1 = new Promise((resolve, _) => {
                 resolve(
                     Catalogo.aggregate()
                         .match({ 'isActive': true })
@@ -41,7 +65,7 @@ module.exports =  {
                 )
         })
 
-        const p2 = new Promise((resolve, reject) => {
+        const p2 = new Promise((resolve, _) => {
             resolve(Catalogo.countDocuments({ "isActive": true }, function( err, count) {
                 return count
             }))
