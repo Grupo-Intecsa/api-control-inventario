2adc2a70d7fb72687baa35ab6720a74d618f11b5
Carlos A. Chávez Rosario <66329126+cchavezmx@users.noreply.github.com>
2021-05-24 15:20:49 -0500

    cambios en el srv de mongo

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const NODE_ENV = process.env.NODE_ENV
 
 const config = {
     test: {
-        MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@gti.tcrun.mongodb.net/test?retryWrites=true&w=majority`
+        MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@gti-shard-00-00.tcrun.mongodb.net:27017,gti-shard-00-01.tcrun.mongodb.net:27017,gti-shard-00-02.tcrun.mongodb.net:27017/test?ssl=true&replicaSet=atlas-tyqnx6-shard-0&authSource=admin&retryWrites=true&w=majority`
     },
     prod: {
         MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@gti.tcrun.mongodb.net/prod?retryWrites=true&w=majority`
