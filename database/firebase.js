cf49813ba6afb8c7dc2586fdf64a65f600e76af2
cchavezmx99 <carlos.chavez@99minutos.com>
2023-04-15 04:53:18 -0600

    cambios en el dese

--- database/firebase.js
+++ database/firebase.js
@@ -7,9 +7,9 @@ const { getFirestore } = require("firebase/firestore");
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const config = JSON.parse(process.env.FIREBASE_CONFIG)
+const config = process.env.FIREBASE_CONFIG
 const firebaseConfig = {
-  ...config,
+  ...JSON.parse(config)
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
