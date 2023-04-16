0245be75ed0fde0821a9c64639afe7ea855915b2
cchavezmx99 <carlos.chavez@99minutos.com>
2023-04-15 20:02:41 -0600

    se cambio config firebase

--- database/firebase.js
+++ database/firebase.js
@@ -7,9 +7,14 @@ const { getFirestore } = require("firebase/firestore");
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const config = process.env.FIREBASE_CONFIG
 const firebaseConfig = {
-  ...JSON.parse(config)
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
