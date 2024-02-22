f88c84ac22e6cf8b987d635c178e7ed1c33e0337
cchavezmx99 <carlos.chavez@99minutos.com>
2024-02-22 17:29:03 -0600

    mejora en la aggregation de vehiculos para se vea en la tabla

--- util/aggregatios.js
+++ util/aggregatios.js
@@ -73,72 +73,155 @@ const viewDocumentsCanceled = (id) => {
 const viewDocumentsNormal = (id) => {
   return [
     {
-      '$match': {
-        '_id': id
+      $match: {
+        _id: id
       }
-    }, 
+    },
     {
-      '$lookup': {
-        'from': 'fletes', 
-        'let': { 'bussinessCostId': '$_id' },
-        'pipeline': [
+      $lookup: {
+        from: "traslados",
+        let: { "bussinessCostId": "$_id" },
+        pipeline: [
           {
-            '$unwind': '$bussiness_cost'
+            $match: {
+              $expr: {
+                $and: [
+                  { '$eq': ['$bussiness_cost', '$$bussinessCostId'] },
+                  { '$eq': ['$isCancel_status', null] }
+                ]
+              }
+            }
           },
           {
-            '$match': {
-              '$expr': {
-                '$and': [
-                  { '$eq': ['$bussiness_cost', '$$bussinessCostId'] },
-                  { '$eq': ['$isCancel_status', null] }
-                ]
-              }
+            $lookup: {
+              from: 'flotillas',
+              let: { flotillaId: "$vehicle" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ['$placas', '$$flotillaId']
+                    }
+                  }
+                },
+                {
+                  $project: {
+                    modelo: 1,
+                    expiration_card: 1,
+                    expiration_verify: 1
+                  }
+                }
+              ],
+              as: 'vehicle_info'
             }
-          }
-        ],
-        'as': 'fletes'
-      }
-    }, 
-    {
-      '$lookup': {
-        'from': 'traslados', 
-        'let': { 'bussinessCostId': '$_id' },
-        'pipeline': [
+          },
           {
-            '$match': {
-              '$expr': {
-                '$and': [
-                  { '$eq': ['$bussiness_cost', '$$bussinessCostId'] },
-                  { '$eq': ['$isCancel_status', null] }
-                ]
-              }
+            $unwind: {
+              path: "$vehicle_info",
+              preserveNullAndEmptyArrays: true
             }
           }
         ],
-        'as': 'traslado'
+        as: "traslados"
       }
-    }, 
+    },
     {
-      '$lookup': {
-        'from': 'rentas',
-        'let': { 'bussinessCostId': '$_id' },
-        'pipeline': [
+      $lookup: {
+        from: "fletes",
+        let: { "bussinessCostId": "$_id" },
+        pipeline: [
           {
-            '$match': {
-              '$expr': {
-                '$and': [
+            $match: {
+              $expr: {
+                $and: [
                   { '$eq': ['$bussiness_cost', '$$bussinessCostId'] },
                   { '$eq': ['$isCancel_status', null] }
                 ]
               }
             }
+          },
+          {
+            $lookup: {
+              from: 'flotillas',
+              let: { flotillaId: "$vehicle" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ['$placas', '$$flotillaId']
+                    }
+                  }
+                },
+                {
+                  $project: {
+                    modelo: 1,
+                    expiration_card: 1,
+                    expiration_verify: 1
+                  }
+                }
+              ],
+              as: 'vehicle_info'
+            }
+          },
+          {
+            $unwind: {
+              path: "$vehicle_info",
+              preserveNullAndEmptyArrays: true
+            }
           }
         ],
-        'as': 'rentas'
+        as: "fletes"
+      }
+    },
+    {
+      $lookup: {
+        from: "rentas",
+        let: { "bussinessCostId": "$_id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $and: [
+                  { '$eq': ['$bussiness_cost', '$$bussinessCostId'] },
+                  { '$eq': ['$isCancel_status', null] }
+                ]
+              }
+            }
+          },
+          {
+            $lookup: {
+              from: 'flotillas',
+              let: { flotillaId: "$vehicle" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ['$placas', '$$flotillaId']
+                    }
+                  }
+                },
+                {
+                  $project: {
+                    modelo: 1,
+                    expiration_card: 1,
+                    expiration_verify: 1
+                  }
+                }
+              ],
+              as: 'vehicle_info'
+            }
+          },
+          {
+            $unwind: {
+              path: "$vehicle_info",
+              preserveNullAndEmptyArrays: true
+            }
+          }
+        ],
+        as: "rentas"
       }
     }
-  ]
-  
+  ]  
 }
 
 
